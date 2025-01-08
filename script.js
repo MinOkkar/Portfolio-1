f83a3c548e4e5825bf4042ba681eb881ac680283
Okkar <minokkar11250@gmail.com>
2025-01-09 06:14:29 +0700

    final product

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  const name = document.getElementById("name").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const message = document.getElementById("message").value.trim();
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
 
-  if (!name || !email || !message) {
+    if (!name || !email || !message) {
+      document.getElementById(
+        "responseMessage"
+      ).textContent = `Field not completed`;
+      return;
+    }
+
     document.getElementById(
       "responseMessage"
-    ).textContent = `Field not completed`;
-    return;
-  }
-
-  document.getElementById(
-    "responseMessage"
-  ).textContent = `Thank you, ${name}. Your message has been sent.`;
-  this.reset();
-});
+    ).textContent = `Thank you, ${name}. Your message has been sent.`;
+    this.reset();
+  });
